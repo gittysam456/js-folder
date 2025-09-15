254d5373d8c1dd4a6da4edf36ed01d9755e59858
gittysam456 <sadafs8798@gmail.com>
2025-09-15 09:17:42 +0530

    Improves form data handling and user response output
    Refactors POST request logic to separately handle first and last name fields
    and dynamically displays user input on the response page.
    Enhances clarity and usability of submitted form data.

--- varfile.js
+++ varfile.js
@@ -6,8 +6,10 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.get('/index.html', function (req, res){
 res.sendFile( __dirname + "/" + "index.html" );
 })
-app.post('/process-post', urlencodedParser, function (req, res){
-    response={
-        first name:req.body.first_name,
-    }
+app.post('/process-post', urlencodedParser, function (req, res)
+{
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    res.write(`<h1>Your first name is: ${fname} </h1>`);
+    res.write(`<h1>Your last name is: ${lname} </h1>`);
 })
